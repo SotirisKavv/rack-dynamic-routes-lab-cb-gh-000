d6102ede5d1fb4675147cf0359b637c49fb5c1e8
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-22 10:40:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
     if req.path.match(/items/)
       if @@items.include? req.path.split(/items/).last
         resp.write "#{req.path.split(/items/).last.price}"
+        resp.status = 200
       else
         resp.write "Item not found"
         resp.status = 400
