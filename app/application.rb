6c681663ce6237c4f984e7e5147742caf64ab519
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-22 10:34:03 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      if @@items.include? req.path.split(/items/).last
+        resp.write req.path.split(/items/).last.price   
+      else
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+end
